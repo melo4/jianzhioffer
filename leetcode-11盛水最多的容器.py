e428c00b5c7718c7bfb88904a35d6b1e44faa9c5
Meng Xiao <871379621@qq.com>
2019-06-03 20:25:33 +0800

    commit

---
+++ leetcode-11盛水最多的容器.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/6/3 20:00
+# @Author  : Meng Xiao
+class Solution:
+    def maxArea(self, height):
+        ans = left = 0
+        right = len(height) - 1
+        while left < right:
+            ans = max(ans, (right-left) * min(height[left], height[right]))
+            if height[left] <= right:
+                left += 1
+            else:
+                right -= 1
+            return ans
