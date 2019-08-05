10d3f2f0c9f9d3bfff4a2042b257cb6f10bd6028
Meng Xiao <871379621@qq.com>
2019-08-05 20:05:24 +0800

    commit

---
+++ 消消乐.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/5 19:57
+# @Author  : Meng Xiao
+
+# 消消乐升级，只要有相同连续数字，全部消除，如1,2,3,3,3,2,3，返回1,3
+def xiaoxiaole(a):
+    stack = []
+    i = 0
+    lena = len(a)
+    while(i<lena):
+        flag = False
+        while(i<lena and len(stack)>0 and stack[-1] == a[i]):
+            i += 1
+            flag = True
+        if(flag):
+            stack.pop()
+        else:
+            stack.append(a[i])
+            i += 1
+
+    return stack
+
+if __name__ == "__main__":
+    a = [2,2,2]
+    s = xiaoxiaole(a)
+    print(s)
