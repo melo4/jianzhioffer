642a6746114d5179ecd02e7d90a7124a3bf2b0e6
melo4 <871379621@qq.com>
2019-09-14 23:11:25 +0800

    commit

--- leetcode-135分发糖果.py
+++ leetcode-135分发糖果.py
@@ -2,3 +2,18 @@
 # @Time    : 2019/8/16 下午11:59
 # @Author  : Meng Xiao
 
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        if not ratings:
+            return 0
+        left = [1 for _ in range(len(ratings))]
+        right = left[:]
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i - 1]:
+                left[i] = left[i - 1] + 1
+        count = left[-1]
+        for j in range(len(ratings) - 2, -1, -1):
+            if ratings[j] > ratings[j + 1]:
+                right[j] = right[j + 1] + 1
+            count += max(left[j], right[j])
+        return count
